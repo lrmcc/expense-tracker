a9827d23b540b3d5e8101ef5ff40d851ed4fb358
lrmcc <lmcconnelldev@gmail.com>
2020-12-07 11:46:34 -0700

    implemented appendChild to Table; regex check amount

--- expense_list.js
+++ expense_list.js
@@ -1,84 +1,63 @@
 let entryCount = 1;
 let entries = [];
 
-let listItemString = ["<tr onclick='expenseListItemDone(this)' id=","</tr>"];
-let emptyTable = "<tr><th>Name</th><th>Date</th><th>Amount</th><th>Actions</th></tr>"
-let defaultTable = emptyTable + "<tr><td>No Entries!</td><th>yyyy-mm-dd</th><td>$0</td><td></td></tr><tr></tr><tr></tr>";
-
-function submitWithReturn(event) {
-    if (event.keyCode === 13) {
-        getExpense();
-       }
-  }
+let emptyTable = "<tr><th>Name</th><th>Date</th><th>Amount</th><th></th></tr>"
+let defaultTable = emptyTable + "<tr><td>No Entries!</td><td>yyyy-mm-dd</td><td>$0</td><td></td></tr>";
 
 function getExpense(){
     let expenseName = document.getElementById("expense-name-text").value;
     let expenseDate = document.getElementById("expense-date").value;
     let expenseAmount = document.getElementById("expense-amount").value;
-    let expenseTuple = [expenseName, expenseDate, expenseAmount];
-    entries.push(expenseTuple);
-    putExpense(expenseTuple);
-    let input = document.getElementById("expense-name-text");
-    input.value = "";
+    if (checkAmount(expenseAmount)){
+        let expenseTuple = [expenseName, expenseDate, expenseAmount];
+        entries.push(expenseTuple);
+        putExpense(expenseTuple);
+        clearExpenseInput();}
+    else{
+        alert("Try Again: Invalid 'Amount' Entry");
+    }
+    
 } 
 
 function putExpense(expenseTuple){
     entryCount = entries.length;
-    console.log("Entry Count: " + entryCount);
-    var completelist = document.getElementById("expense-list");
-    if (entryCount === 1){
-        completelist.innerHTML = emptyTable;
-    }
-    completelist.innerHTML +=  listItemString[0] + entryCount +"><td>"  + expenseTuple[0] + "</td><td>" + expenseTuple[1]+ "</td><td>$" + expenseTuple[2] + "</td><td>" + getClearItem(entryCount) + getDoneItem(entryCount) +"</td>" + listItemString[1];
-    console.log("clear Item: " + getClearItem(entryCount));
-} 
-
-function expenseListItemDone(item) {
-    let txtDec = item.style.textDecoration;
-    if (txtDec === "line-through"){
-        item.style = "text-decoration:none;";
-    }else{
-      item.style = "text-decoration:line-through;";  
-    }
-    
-  }
-
-function getClearItem(count){
-    return "<button type='button' class='clear-expense-item-from-list-button' id=" + entryCount +" onclick='clearExpenseItemFromList(" + entryCount + ")'>x</button>";
+    var expenseTable = document.getElementById("expense-table");
+    if (entryCount === 1) expenseTable.innerHTML = emptyTable;
+    addTableRow(entryCount, expenseTuple, expenseTable);
 }
 
-function getDoneItem(count){
-    return "<button type='button' class='expense-list-item-done-button' id=" + entryCount +" onclick='expenseListItemDone(" + entryCount + ")'>Done</button>"
-}
-
-function clearExpenseItemFromList(itemEntryCount){
-    console.log("target item count is: " + itemEntryCount);
+function clearExpenseItemFromTable(itemEntryCount){
     entries = entries.filter(a => a !== entries[itemEntryCount-1]);
-    console.log(entries);
-    var completelist = document.getElementById("expense-list");
-    completelist.innerHTML = emptyTable;
-    entryCount = 1;
-    for (let i = 0; i < entries.length; i++){
-        completelist.innerHTML += listItemString[0] + entryCount +"><td>"  + entries[i][0] + "</td><td>" + entries[i][1]+ "</td><td>$" + entries[i][2] + "</td><td>" + getClearItem(entryCount) + getDoneItem(entryCount) +"</td>" + listItemString[1];
-        entryCount++;
-    }
     entryCount = entries.length;
+    var expenseTable = document.getElementById("expense-table");
     if (entryCount === 0){
-        completelist.innerHTML = defaultTable;
+        expenseTable.innerHTML = defaultTable;
+    }
+    else{
+        expenseTable.innerHTML = emptyTable;
+        for (let i = 0; i < entries.length; i++){
+            let expenseTuple = [entries[i][0], entries[i][1], entries[i][2]];
+            addTableRow((i+1), expenseTuple, expenseTable);
+        }
     }
 }
 
-function clearExpenseList(){
-    let choice = confirm("Are you sure you want to clear the list?");
-    if (choice){
-        var completelist = document.getElementById("expense-list");
-        entries = [];
-        entryCount = 0;
-        completelist.innerHTML = defaultTable;
+function addTableRow(entryCount, expenseTuple, expenseTable){
+    var row = document.createElement("tr");
+    row.id = entryCount;
+    expenseTable.appendChild(row);
+    for (var i = 0; i < 4; i++) {
+        var cell = document.createElement("td");
+        if (i<3){
+            cell.innerHTML = expenseTuple[i];
+        }else{
+            cell.innerHTML = getClearItemButton(entryCount)
+        }
+        row.appendChild(cell)
     }
-    
 }
-function exportExpenseList(){
+
+function exportExpenseTable(){
     let csvContent = "data:text/csv;charset=utf-8," + entries.toString();
     var encodedUri = encodeURI(csvContent);
     var link = document.createElement("a");
@@ -86,4 +65,42 @@ function exportExpenseList(){
     link.setAttribute("download", "expense_data.csv");
     document.body.appendChild(link);
     link.click(); 
+}
+
+function clearExpenseTable(){
+    let choice = confirm("Are you sure you want to clear the table?");
+    if (choice){
+        var expenseTable = document.getElementById("expense-table");
+        entries = [];
+        entryCount = 0;
+        expenseTable.innerHTML = defaultTable;
+    }
+    
+}
+function checkAmount(expenseAmount){
+    let isnum = /^\d+$/.test(expenseAmount);
+    console.log(isnum);
+    return (isnum)
+}
+
+function submitWithReturn(event) {
+    if (event.keyCode === 13) {
+        getExpense();
+       }
+  }
+function clearExpenseInput(){
+    let nameInput = document.getElementById("expense-name-text");
+    console.log("nameInput: " + nameInput);
+    let numberInput = document.getElementById("expense-amount");
+    console.log("numberInput: " + numberInput);
+    let dateInput = document.getElementById("expense-date");
+    console.log("dateInput: " + dateInput);
+    nameInput.value = "";
+    numberInput = "";
+    dateInput = "";
+
+}
+
+function getClearItemButton(count){
+    return "<button type='button' class='clear-expense-item-from-table-button' id=" + count +" onclick='clearExpenseItemFromTable(" + count + ")'>x</button>";
 }
